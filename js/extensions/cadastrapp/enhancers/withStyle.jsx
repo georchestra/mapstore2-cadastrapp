c32195f44fc36cd327a57839abb09d64066720c3
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-10-13 16:39:04 +0200

    Some doc about css loading

--- js/extensions/cadastrapp/enhancers/withStyle.jsx
+++ js/extensions/cadastrapp/enhancers/withStyle.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 /**
- * Adds a style tag with the CSS inline.
+ * Adds the style passed as string in a style tag near the component
+ * This workarounds limitation of extensions to load one single bundle
+ * and allow to use a separated file for css, using it in couple with `raw-loader`.
  * @param {string} style the style to set inline
+ * @example
+ * import css from 'raw-loader!../cadastrapp.css.txt'; // the extension must NOT be .css
+ * withStyle(css)(Component); // this enhances the component with the style
+ *
  */
 const withStyle = (style) => (Component) =>
     (props) => {
