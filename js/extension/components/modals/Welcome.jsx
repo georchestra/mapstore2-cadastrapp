5d7f70dadb896185bceca2192b5a1f9b606c9653
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2021-06-04 12:03:06 +0200

    Fixed style

--- js/extension/components/modals/Welcome.jsx
+++ js/extension/components/modals/Welcome.jsx
@@ -23,14 +23,14 @@ export default function WelcomeMessage(props) {
                 <h3 className="welcome-message-items"><Message msgId={'cadastrapp.title'}/></h3>
                 <h4 className="welcome-message-items"><Message msgId={'cadastrapp.selectTool'}/></h4>
             </div>
-            <div style={{padding: 40}}>
-                <h4 className="welcome-message-items"><Message msgId={'cadastrapp.dateValiditeEDIGEO'}/>: &nbsp;{dateValiditeEDIGEO}</h4>
-                <h4 className="welcome-message-items"><Message msgId={'cadastrapp.dateValiditeMajic'}/>: &nbsp;{dateValiditeMajic}</h4>
-            </div>
-            <h4 className="welcome-message-build">build &nbsp;
+            <table className="welcome-message-versions">
+                <tr><td><Message msgId={'cadastrapp.dateValiditeEDIGEO'}/>: </td><td>{dateValiditeEDIGEO}</td></tr>
+                <tr><td><Message msgId={'cadastrapp.dateValiditeMajic'}/>: </td><td>{dateValiditeMajic}</td></tr>
+            </table>
+            <div className="welcome-message-build">build &nbsp;
                 {/* eslint-disable-next-line no-undef */}
-                <a href={`${__REPOURL__?.replace('.git', '')}/commit/${__COMMITHASH__}`}>{__COMMITHASH__?.substr(0, 8)}</a>
-            </h4>
+                <a target="_blank" href={`${__REPOURL__?.replace('.git', '')}/commit/${__COMMITHASH__}`}>{__COMMITHASH__?.substr(0, 8)}</a>
+            </div>
         </div>
     );
 }
