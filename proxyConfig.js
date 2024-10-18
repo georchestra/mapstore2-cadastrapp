d3c7e3989e6d0559be91b68c88ae265470db0663
Lorenzo Natali <lorenzo.natali@geosolutionsgroup.com>
2024-10-18 18:42:29 +0200

    Update build and tests files to work properly

--- proxyConfig.js
+++ proxyConfig.js
@@ -2,7 +2,7 @@
 
 const DEV_PROTOCOL = "http";
 const DEV_HOST = "localhost:8080";
-const cookie = "_hp2_props.2826793817=%7B%22account_state%22%3A%22active%22%7D; _hp2_id.2826793817=%7B%22userId%22%3A%221499114905535127%22%2C%22pageviewId%22%3A%222428177607153135%22%2C%22sessionId%22%3A%222201495479166025%22%2C%22identity%22%3A%223635521%22%2C%22trackerVersion%22%3A%224.0%22%2C%22identityField%22%3Anull%2C%22isIdentified%22%3A1%7D; _ga=GA1.2.418248975.1607951243; JSESSIONID=node0zn0cohqg5jw2qn4nddqynxiw70196.node0";
+const cookie = "JSESSIONID=node0fieai9vvjzcv1uhtlkbbmp7jt4043608.node0";
 module.exports = {
     "/rest": {
         target: `${DEV_PROTOCOL}://${DEV_HOST}/mapstore`,
@@ -26,11 +26,12 @@ module.exports = {
         }
     },
     "/proxy": {
-        target: `https://dev.mapstore.geo-solutions.it/mapstore`,
+        target: `${DEV_PROTOCOL}://${DEV_HOST}/mapstore/`,
         secure: false
     },
     "/mapstore/proxy": {
-        target: `https://dev.mapstore.geo-solutions.it`,
+        target: `${DEV_PROTOCOL}://${DEV_HOST}`,
+
         secure: false
     },
     "/geonetwork": {
