4e423cdf3b458bf0518b3687b44ca5c41aeb0106
Loïc Poincelet <loic.poincelet@jdev.fr>
2024-12-20 11:10:14 +0100

    remove cadastrapp icon to create a specific branch for issue 175

--- js/extension/plugins/Extension.jsx
+++ js/extension/plugins/Extension.jsx
@@ -7,7 +7,6 @@ import Message from "@mapstore/components/I18N/Message";
 import Main from './cadastrapp/Main';
 import init from '../enhancers/init';
 import { CONTROL_NAME } from '../constants';
-import cadastrapp_icon from '../../../assets/img/cadastrapp_icon.svg';
 
 import {setUp} from '../actions/cadastrapp';
 
@@ -48,14 +47,14 @@ export default {
             name: "cadastrapp",
             position: 1050,
             text: <Message msgId="cadastrapp.title"/>,
-            icon: <img src={ cadastrapp_icon } className="cadastrappIcon"/>,
+            icon: <Glyphicon glyph="th" />,
             doNotHide: true,
             action: toggleControl.bind(null, CONTROL_NAME, null),
             priority: 2
         },
         SidebarMenu: {
             name: "cadastrapp",
-            icon: <img src={ cadastrapp_icon } className="cadastrappIcon"/>,
+            icon: <Glyphicon glyph="th" />,
             tooltip: "cadastrapp.title",
             text: <Message msgId="cadastrapp.title"/>,
             doNotHide: true,
